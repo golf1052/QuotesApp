96e873be58d2ad789f2cbd9847b65eabefe98bae
Nat Dempkowski <natdempk@gmail.com>
2014-09-12 16:33:20 -0400

    Web: Display Quotes in a stream

--- Web/js/app.js
+++ Web/js/app.js
@@ -15,22 +15,23 @@
 			var pUrl = 'https://api.parse.com/1/'; 
 
 			// get room data
-			$http.get(pUrl + 'classes/Room/RN7ATsC59V').success(function(data){
+			$http.get(pUrl + 'classes/Room/RN7ATsC59V').success(function(data){ // for party
+			//$http.get(pUrl + 'classes/Room/SwU8fgU2iE').success(function(data){ // for testing w/ more quotes
 				quoteStream.room = data;
 				quoteStream.quotes = quoteStream.room['quotes'];
-				console.log(quoteStream.quotes);
+				//console.log(quoteStream.quotes);
 				// go through the quote IDs and get their objects
 				for (var i = 0; i < quoteStream.quotes.length; i++) {
 					!function outer(ii){ // wrapper because for loop
 						// get Quote objects from Parse
-						$http.get(pUrl + '/classes/Quote/' + quoteStream.quotes[i]['objectId']).success(function(data) {
+						$http.get(pUrl + 'classes/Quote/' + quoteStream.quotes[i]['objectId']).success(function(data) {
 								quoteStream.quotes[ii] = data;
 								for (var j = 0; j < quoteStream.quotes[ii]['blurbs'].length; j++) {
 									!function outerj(jj){ // another wrapper
 										// get Blurb objects from Parse
-										$http.get(pUrl + '/classes/Blurb/' + quoteStream.quotes[ii]['blurbs'][j]['objectId']).success(function(data) {
+										$http.get(pUrl + 'classes/Blurb/' + quoteStream.quotes[ii]['blurbs'][j]['objectId']).success(function(data) {
 											// popular Blurb objects to data
-											quoteStream.quotes[ii][jj] = data;
+											quoteStream.quotes[ii]['blurbs'][jj] = data;
 									})
 								}(j)
 								}
