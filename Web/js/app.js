63ecf9b06ff2b708a938f4dd82600841ee52b24e
Nat Dempkowski <natdempk@gmail.com>
2014-09-12 19:50:08 -0400

    Web: Make submitting + autoreload hackily work

--- Web/js/app.js
+++ Web/js/app.js
@@ -1,5 +1,7 @@
 (function() {
-	var app = angular.module('quotesApp', []);
+	var app = angular.module('quotesApp', ['ngRoute']);
+
+	var prodRoom = 'RN7ATsC59V';
 
 	app.controller('QuoteStreamController', ['$http', function($http) {
 			// initialize data structures for room + quote data
@@ -15,28 +17,19 @@
 			var pUrl = 'https://api.parse.com/1/'; 
 
 			// get room data
-			$http.get(pUrl + 'classes/Room/RN7ATsC59V').success(function(data){ // for party
+			$http.get(pUrl + 'classes/Room/' + prodRoom).success(function(data){ // for party
 			//$http.get(pUrl + 'classes/Room/SwU8fgU2iE').success(function(data){ // for testing w/ more quotes
 				quoteStream.room = data;
 				quoteStream.quotes = quoteStream.room['quotes'];
-				//console.log(quoteStream.quotes);
+				//console.log(quoteStream);
 				// go through the quote IDs and get their objects
+				// TODO: QUOTE AGE ORDERING, I THINK ITS WRONG ATM
 				for (var i = 0; i < quoteStream.quotes.length; i++) {
 					!function outer(ii){ // wrapper because for loop
 						// get Quote objects from Parse
 						$http.get(pUrl + 'classes/Quote/' + quoteStream.quotes[i]['objectId']).success(function(data) {
 								quoteStream.quotes[ii] = data;
-								for (var j = 0; j < quoteStream.quotes[ii]['blurbs'].length; j++) {
-									!function outerj(jj){ // another wrapper
-										// get Blurb objects from Parse
-										$http.get(pUrl + 'classes/Blurb/' + quoteStream.quotes[ii]['blurbs'][j]['objectId']).success(function(data) {
-											// popular Blurb objects to data
-											quoteStream.quotes[ii]['blurbs'][jj] = data;
-									})
-								}(j)
-								}
-							}
-							)
+							});
 					}(i)
 				}
 
@@ -46,4 +39,72 @@
 
 
 	} ]);
+
+	app.controller('QuoteSubmitController', ['$scope', '$route', '$window', '$http', function($scope, $route, $window, $http) {
+		// var foo = "";
+		this.quote = {
+			"room" : { "__type"    : "Pointer",
+				"className" : "Room",
+				"objectId"  : prodRoom,
+			},
+			"submitter" : { "__type"    : "Pointer",
+				"className" : "_User",
+				"objectId"  : "wwQKl03DKN",
+			},
+			"blurbs" : [
+				// { 'blurb' : 'foo', 'attributed' : 'bar' }
+			]
+		};
+
+		this.parseQuote = {};
+		this.room = {};
+
+		$http.defaults.headers.common = {
+			'X-Parse-Application-Id' : 'i0Js9aOJTnuUygn0yQLZyy4L4EWbacRmMoqu2IXC',
+			'X-Parse-REST-API-Key' : '61B0s035wFn2PhVNX9sL8pPKFro9iIzuSaohyCwL'
+		};
+		var pUrl = 'https://api.parse.com/1/'; 
+
+		this.addQuote = function() {
+			// submit quote to parse
+			$http.post(pUrl + 'classes/Quote', this.quote).success(function(data){
+				this.quote = data;
+				var quoteVar = this.quote;
+				console.log("this.quote")
+				console.log(this.quote);
+								console.log("this.quote['objectId']")
+				console.log(this.quote['objectId']);
+
+				$http.get(pUrl + 'classes/Room/' + prodRoom).success(function(data){
+					this.room = data;
+					this.pushData = {
+						'__type' : "Pointer",
+						'className' : 'Quote',
+						'objectId' : quoteVar['objectId'],
+					}
+					this.room['quotes'].push(this.pushData);
+										console.log(this.room);
+					$http.put(pUrl + 'classes/Room/' + this.room['objectId'], this.room).success(function(data){
+						this.reponse = data;
+						$window.location.href="file:///C:/Users/Nat/Documents/GitHub/QuotesApp/Web/index.html"
+					});
+				});
+
+				// then reset quote object?
+				this.quote = {
+						"room" : { "__type"    : "Pointer",
+							"className" : "Room",
+							"objectId"  : prodRoom,
+						},
+						"submitter" : { "__type"    : "Pointer",
+							"className" : "_User",
+							"objectId"  : "wwQKl03DKN",
+						},
+						"blurbs" : [
+							// { 'blurb' : 'foo', 'attributed' : 'bar' }
+						]
+					};
+		});
+
+	} } ]);
 })();
